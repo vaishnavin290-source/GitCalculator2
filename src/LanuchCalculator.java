9444647f5ba4b3898f1aff18dac26f7321b27f5e
Vaishnavi Nayak <vaishnavin290@gmail.com>
2026-08-22 09:34:09 +0530

    this is new version

--- src/LanuchCalculator.java
+++ src/LanuchCalculator.java
@@ -1,91 +1,31 @@
 import java.util.Scanner;
-public class LanuchCalculator {
 
+class LanuchCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void addition() {
-		Scanner scan =new Scanner(System.in);
-		System.out.println("Enter first Number");
-		int a= scan.nextInt();
-		System.out.println("Enter second  Number");
-		int b= scan.nextInt();
-		int c=a+b;
-		System.out.println(c);
-		// TODO Auto-generated method stub
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-	}
-	
-	public static void substraction() {
-		Scanner scan =new Scanner(System.in);
-		System.out.println("Enter first Number");
-		int a= scan.nextInt();
-		System.out.println("Enter second  Number");
-		int b= scan.nextInt();
-		int c=a-b;
-		System.out.println(c);
-		// TODO Auto-generated method stub
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
 
-	}
-	
-	public static void multiplication() {
-		Scanner scan =new Scanner(System.in);
-		System.out.println("Enter first Number");
-		int a= scan.nextInt();
-		System.out.println("Enter second  Number");
-		int b= scan.nextInt();
-		int c=a*b;
-		System.out.println(c);
-		// TODO Auto-generated method stub
-
-	}
-	public static void divison() {
-		Scanner scan =new Scanner(System.in);
-		System.out.println("Enter first Number");
-		int a= scan.nextInt();
-		System.out.println("Enter second  Number");
-		int b= scan.nextInt();
-		int c=a/b;
-		System.out.println(c);
-		// TODO Auto-generated method stub
-
-	}
-	
-	public static void main(String[] args) {
-
-	    Scanner scan = new Scanner(System.in);
-
-	    System.out.println("1. Addition");
-	    System.out.println("2. Subtraction");
-	    System.out.println("3. Multiplication");
-	    System.out.println("4. Division");
-	    System.out.print("Enter your choice: ");
-
-	    int choice = scan.nextInt();
-
-	    switch (choice) {
-	        case 1:
-	            addition();
-	            break;
-
-	        case 2:
-	            substraction();
-	            break;
-
-	        case 3:
-	            multiplication();
-	            break;
-
-	        case 4:
-	            divison();
-	            break;
-
-	        default:
-	            System.out.println("Invalid Choice");
-	    }
-
-	    
-	}
-
-
-
-
+        switch (op) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
 }
